18c207af6504a569f6c7f69c35e41583611a56fc
swarit <dholakia.swarit@gmail.com>
2021-12-10 23:40:48 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
 // swarit dholakia - 251107834 - sdholaki@uwo.ca
 //se3316 lab5
 
-//setup
-//-----
-const express = require ('express');
-let jobs_in_json = require ('./jobs.json');
+const express = require("express");
+let jobsList = require("./jobs.json");
+
 const app = express();
-app.use(express.static('htmls'));
-//-----
 
 //q1:
 //-----
 // create empty object; for each category in job for each job in json, count if not the same job if not in created object already, and convert back to json
-app.get('/ListAllCategories', (req,res) => {
-        let categories = {};      
-        for (let j in jobs_in_json)
-        {   
-            for (let c of jobs_in_json[j].categories)
-                if (!categories[c])
-                {
-                    categories[c] = 1;   
-                }
-                else
-                    categories[c]++;
-        }
-        res.json(categories);
-    
-    });
-//-----
+app.get("/jobCategories", (req, res) => {
 
-//q2:
-//-----
-//new array to hold categories, iterate over json of jobs; if in parameters provided, append to array, then convert and send as json
-app.get('/:TheCategory', (req,res) =>{
-    let jobsInCategories =[];  
-    for(let j in jobs_in_json)
-    {
-        if(jobs_in_json[j].categories.includes(req.params.category))
-        {
-            jobsInCategories.push(j);
-        }
+  let categories = {};
+
+  for (let j in jobsList) {
+
+    for (let c of jobsList[j].categories) {
+      
+      if (!categories[c]) categories[c] = 1;
+      
+      else categories[c]++;
     }
-    res.json({
-        jobs: jobsInCategories
-    })
+  }
+
+  res.json(categories);
 });
 //-----
 
 //q3:
 //-----
 // new array to hold jobs, find city name in job title, push city to new array, convert and send as json
-app.get('/ListAllJobCities', (req,res)=> {
+app.get('/jobInCity', (req,res)=> {
     let jobsincity = [];
-    for (let j in jobs_in_json)
+
+    for (let job in jobsList)
     {
-        if (jobs_in_json[j].title.includes(req.query.city))
+        if (jobsList[job].title.includes(req.query.city))
         {
-            jobsincity.push(j);   
+            jobsincity.push(job);   
         }
     }
     res.json(
         {
-            jobs: jobsincity
+            ListAllJobCities: jobsincity
         }
     );
 });
 //-----
 
-//live @ port 80
-app.listen(80);  
+//q2:
+//-----
+//new array to hold categories, iterate over json of jobs; if in parameters provided, append to array, then convert and send as json
+app.get("/:category", (req, res) => {
+  
+  let jobs = [];
+
+  for (let j in jobsList) {
+    
+    if (jobsList[j].categories.includes(req.params.category)) jobs.push(j);
+  }
+
+  res.json({ jobs: jobs });
+});
+
+app.listen(80);
